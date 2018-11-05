888812afd45ab27b186f4864954ba760898a3a69
Bart <Bartosz1Dobrowolski@gmail.com>
2018-11-05 20:52:24 +0100

    Init commit, added .gitignore and pom.xml and the main class of an application.

---
+++ src/main/java/hotelAPI/HotelApiApp.java
@@ -0,0 +1,14 @@
+package hotelAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelApiApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(HotelApiApp.class, args);
+	}
+
+}
